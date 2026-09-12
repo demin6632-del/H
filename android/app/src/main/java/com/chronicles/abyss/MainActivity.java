f6a09625841eaae4ebda596d47d6674c51df9cb6
demin6632-del <demin6632@gmail.com>
2026-09-12 15:58:15 +0500

    Add Android WebView MainActivity for game

---
+++ android/app/src/main/java/com/chronicles/abyss/MainActivity.java
@@ -0,0 +1,19 @@
+package com.chronicles.abyss;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+}
